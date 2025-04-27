57812abc090e708538fd428525c13a9845b3b023
doraratt <ella.semilla28@gmail.com>
2025-04-27 14:32:09 +0800

    improved UI design in align with the web version

--- MarkahanMobile/app/build.gradle.kts
+++ MarkahanMobile/app/build.gradle.kts
@@ -31,17 +31,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
     packaging {
         resources {
@@ -52,38 +52,33 @@ android {
 
 dependencies {
 
-//    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+    //implementation(libs.mediation.test.suite)
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
     //date
-    implementation ("com.google.android.material:material:1.6.0")
+    implementation ("com.google.android.material:material:1.12.0")
 
     //recycle
-    implementation ("androidx.recyclerview:recyclerview:1.2.1") // or latest version
+    implementation ("androidx.recyclerview:recyclerview:1.3.2") // or latest version
 
     //card
     implementation ("androidx.cardview:cardview:1.0.0")
     implementation ("androidx.activity:activity-ktx:1.8.0")
 
     implementation ("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.core:core:1.12.0")
-
-    //navigation drawer
-    implementation(libs.material)
-    implementation(libs.androidx.appcompat)
 }
